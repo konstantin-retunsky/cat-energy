32de96193af274c74d18365c5756ed1cfadb003b
Konstantin Retunsky <konstantin.retunsky@gmail.com>
2021-04-30 08:02:00 +0500

    remove header in pages template

--- gulp/pathTasks.js
+++ gulp/pathTasks.js
@@ -1,13 +1,13 @@
 
 module.exports = {
   src: {
-    styles: ["./source/global/scss/global_style.scss", "./source/pages/**/*.scss"],
+    styles: "./source/pages/**/*.scss",
     html: "./source/pages/**/*.{njk,html}",
     scripts: "./source/**/*.js",
     images: "./source/**/images/*.{jpg,png,svg,ico}",
     svgSprite: "./source/**/sprite/*.svg",
     fonts: "./source/global/fonts/*",
-  }, 
+  },
   build: {
     styles: "build/css",
     html: "build",
@@ -18,7 +18,7 @@ module.exports = {
   },
   browserSync: {
     build: "build",
-    html: "./source/**/*.{njk,html}", 
+    html: "./source/**/*.{njk,html}",
     styles: "./source/**/*.scss",
     images: "./source/**/images/*.{jpg,png,svg,ico}",
     svgSprite: "./source/**/sprite/*.svg",
